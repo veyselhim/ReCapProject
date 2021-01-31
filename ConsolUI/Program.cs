f8ff80665755601a783fb2d2f5b7026493262bdf
Veysel Himmetoğlu <veyselhim@gmail.com>
2021-01-31 18:20:25 +0300

    Add project files.

---
+++ ConsolUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsolUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Özellikler : {0}\nGünlük Fiyatı : {1}\nModel Yılı : {2}",car.Description,car.DailyPrice,car.ModelYear);
+            }
+        }
+    }
+}
